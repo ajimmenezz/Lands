77536a56ace8fd552b406bfd7019831ba502fbdb
Alonso <ajimmenezz@gmail.com>
2018-09-12 23:51:36 -0500

    Commit Respaldo

--- Lands/Lands/App.xaml.cs
+++ Lands/Lands/App.xaml.cs
@@ -12,7 +12,7 @@ namespace Lands
         {
             InitializeComponent();
 
-            MainPage = new LoginPage();
+            MainPage = new NavigationPage(new LoginPage());
         }
         #endregion
 
