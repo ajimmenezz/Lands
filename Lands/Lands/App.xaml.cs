87df2637433cf699e0ce2bea4e06e301b6fe40f2
Alonso Jiménez <ajimmenezz@outlook.com>
2018-09-12 19:01:32 -0500

    Commit 1

---
+++ Lands/Lands/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Lands
+{
+
+    using Xamarin.Forms;    
+    using Views;
+
+    public partial class App : Application
+    {
+        #region Constructores
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new LoginPage();
+        }
+        #endregion
+
+
+        #region Methods
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+        #endregion
+    }
+}
